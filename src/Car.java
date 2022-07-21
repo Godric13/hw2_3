611b6c9ce3acb4ca88cbb9c8b566e7b7f48e105a
Godric <lasthax@yandex.ru>
2022-07-21 21:33:17 +0300

    hw2_3

---
+++ src/Car.java
@@ -0,0 +1,21 @@
+public class Car extends Transport implements Service {
+    public Car(String modelName, int wheelsCount) {
+            super(modelName, wheelsCount);
+    }
+
+    public void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+        checkEngine();
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
